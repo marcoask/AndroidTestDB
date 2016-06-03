459ca5c72b6d9179573edcefcd31ff998caf2a02
massini <marco.assini@optimo.it>
2016-06-03 17:26:12 +0200

    WIP

---
+++ app/src/main/java/it/assini/example/mydbapplication/db/FeedReaderContract.java
@@ -0,0 +1,35 @@
+package it.assini.example.mydbapplication.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by massini on 03/06/2016.
+ * https://developer.android.com/training/basics/data-storage/databases.html
+ */
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+
+    }
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+         // Any other options for the CREATE command
+            " )";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+}
